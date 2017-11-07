454f90f24f6e9929668d5ce5873d6dc659486730
Federico Stacchietti <federico.stacchietti@gmail.com>
2017-11-07 20:08:07 +0100

    implementazione metodi index e modifica funzioni MemsController

--- sci/Controllers/MemsController.cs
+++ sci/Controllers/MemsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using sci.Models;
 using PagedList;
+using System.Collections;
 
 namespace sci.Controllers
 {
@@ -15,74 +16,28 @@ namespace sci.Controllers
     {
         private sci_newEntities db = new sci_newEntities();
 
-       
-            // GET: Mems
-            public ActionResult Index(string sortOrder, string currentFilter, string searchString,string prefix, int? page)
+        public ActionResult Index()
         {
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Cod" ? "cod_desc" : "Cod";
+            var mems = db.Mem.Select(m => m).Take(50);
+            var data = db.Mem.Select(m => m).ToList();
+            ViewBag.data = data;
 
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewBag.CurrentFilter = searchString;
-            var mems = from m in db.Mem select m;
-
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                if (searchString.All(char.IsDigit))
-                {
-                    mems = mems.Where(m => m.Cod.ToString().Equals(searchString));
-                    if (mems.Count() == 1)
-                    {
-                        var pags = db.Pag.Where(p => p.CodP.ToString().Equals(searchString)).Select(d => d.AnnoP);
-                        if (pags.Any())
-                        {
-                            var last = pags.Max();
-                            var lastcod = Convert.ToInt32(searchString); ;
-                            return RedirectToAction("Edit", "Pags", new { codp = lastcod, annop = last });
-                        }
-                        else
-                            return RedirectToAction("Edit/" + mems.Select(m => m.Cod).Max().ToString(), "Mems", null);
-                    }
-                }
-                     
-                else
-                    mems = mems.Where(m => m.Cognome.Contains(searchString));
-            }
-
-
-            switch (sortOrder)
-            {
-                case "name_desc":
-                    mems = mems.OrderByDescending(m => m.Cognome);
-                    break;
-                case "Cod":
-                    mems = mems.OrderBy(m => m.Cod);
-                    break;
-                case "cod_desc":
-                    mems = mems.OrderByDescending(m => m.Cod);
-                    break;
-                default:  // Name ascending 
-                    mems = mems.OrderBy(m => m.Cognome);
-                    break;
-            }
-
-            int pageSize = 20;
-            int pageNumber = (page ?? 1);
-            return View(mems.ToPagedList(pageNumber, pageSize));
+            return View(mems);
         }
 
-        
+        [HttpPost]
+        public ActionResult Index(string searchString)
+        {
+            ViewBag.CurrentFilter = searchString;
 
-    // GET: Mems/Details/5
-    public ActionResult Details(int? id)
+            var mems = db.Mem.Where(m => (m.Cognome.StartsWith(searchString)) || (m.Nome.StartsWith(searchString)) || (m.Cod.ToString().StartsWith(searchString))).Take(10);
+
+            return View(mems);
+        }
+
+
+        // GET: Mems/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -126,77 +81,6 @@ namespace sci.Controllers
             ViewBag.Sesso = new SelectList(db.TabSex, "CodSex", "DescrSex", mem.Sesso);
             ViewBag.CodTitStu = new SelectList(db.TabTitStu, "TCodTitStu", "DescrTitStu", mem.CodTitStu);
             return View(mem);
-        }
-
-        // GET: Mems/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Mem mem = db.Mem.Find(id);
-            if (mem == null)
-            {
-                return HttpNotFound();
-            }
-
-            // anni dei pagamenti
-            var anniPag = db.Pag.Where(d => d.CodP == id ).Select(d => d.AnnoP.ToString()).ToList();
-            if (anniPag.Any())
-            {
-                ViewBag.anniPag = anniPag;
-            }
-
-            ViewBag.Naz = new SelectList(db.TabNaz, "CodNaz", "DescrNaz", mem.Naz);
-            ViewBag.Sesso = new SelectList(db.TabSex, "CodSex", "DescrSex", mem.Sesso);
-            ViewBag.CodTitStu = new SelectList(db.TabTitStu, "TCodTitStu", "DescrTitStu", mem.CodTitStu);
-            return View(mem);
-        }
-
-        // POST: Mems/Edit/5
-        // Per proteggere da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
-        // Per ulteriori dettagli, vedere http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Cod,Sesso,Nom,Titolo,Nome,Cognome,Ist,Via,CAP,Cit,Naz,DaNa,Tel,Fax,Email,DimAn,NoStampa,Modificato,PresentatoDa,FlagRegalo,PresentatoDa2,FlagRegalo2,AnnoPresentazione,CodTitStu")] Mem mem)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(mem).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Naz = new SelectList(db.TabNaz, "CodNaz", "DescrNaz", mem.Naz);
-            ViewBag.Sesso = new SelectList(db.TabSex, "CodSex", "DescrSex", mem.Sesso);
-            ViewBag.CodTitStu = new SelectList(db.TabTitStu, "TCodTitStu", "DescrTitStu", mem.CodTitStu);
-            return View(mem);
-        }
-
-        // GET: Mems/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Mem mem = db.Mem.Find(id);
-            if (mem == null)
-            {
-                return HttpNotFound();
-            }
-            return View(mem);
-        }
-
-        // POST: Mems/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Mem mem = db.Mem.Find(id);
-            db.Mem.Remove(mem);
-            db.SaveChanges();
-            return RedirectToAction("Index");
         }
 
         protected override void Dispose(bool disposing)
